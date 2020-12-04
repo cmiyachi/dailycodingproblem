bcd8e30cfb4464df51a6645a0379edc62f503ab8
Chris M <cmiyachi@alum.mit.edu>
2020-12-03 19:54:28 -0500

    first program

---
+++ cmake-build-debug/CMakeFiles/AlgoStudy.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/AlgoStudy.dir/main.cpp.o"
+  "CMakeFiles/AlgoStudy.dir/products.cpp.o"
+  "AlgoStudy.pdb"
+  "AlgoStudy.exe"
+  "libAlgoStudy.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/AlgoStudy.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
